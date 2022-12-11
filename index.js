5722d2f8ff03aa2af6fdd1bda70e37523fd1255e
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-12-10 23:27:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+alert("Eu falo, mas não tenho boca. Eu ouço, mas não tenho ouvidos. Não tenho corpo, mas vivo com o vento. Quem sou eu?")
+
+let x= prompt('Digite sua resposta')
+
+if(x==="eco"){
+    alert('Você acertou, seu primeiro número é 6(anote)')
+}
+else if(x==="Eco"){
+    alert('Você acertou , seu primeiro numero é 6(anote)')
+}
+else if(x!==eco){
+    alert('Você errou')
+}
+alert('Esse "enigma" foi apenas uma introdução do que está por vir ')
